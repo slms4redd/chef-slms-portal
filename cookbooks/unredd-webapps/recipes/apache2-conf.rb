36cb5ce1af9fca1eb7dbe85465d7194f98bf74e5
Stefano Giaccio <sgiaccio@Stefanos-iMac.local>
2012-12-11 09:53:27 +0100

    Added mod_proxy configure

--- cookbooks/unredd-webapps/recipes/apache2-conf.rb
+++ cookbooks/unredd-webapps/recipes/apache2-conf.rb
@@ -6,6 +6,14 @@ include_recipe "apache2::mod_proxy"
 
 include_recipe "apache2::mod_proxy_ajp"
 
+# configure mod_proxy
+template "#{node['apache']['dir']}/mods-available/proxy.conf" do
+  source "proxy.conf.erb"
+  notifies :restart, "service[apache2]", :delayed
+  mode 0644
+end
+
+
 web_app "tomcat_proxy" do
   template "unredd_proxy_template.conf.erb"
   owner "apache"
