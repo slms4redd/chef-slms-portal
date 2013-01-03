13bdfa525d07d1d36d807da81de3f50d77c9bde6
Stefano Giaccio <sgiaccio@Stefanos-iMac.local>
2013-01-03 15:26:19 +0100

    Updated unredd-nfms-portal jai recipe to support Ubuntu 64 bit

--- cookbooks/unredd-nfms-portal/recipes/jai.rb
+++ cookbooks/unredd-nfms-portal/recipes/jai.rb
@@ -1,11 +1,25 @@
-ark 'jai' do
-  url    node['unredd-nfms-portal']['jai']['jai_download_url']
+jai_arch             = node['kernel']['machine'] =~ /x86_64/ ? "amd64" : "i586"
+jai_version          = node['unredd-nfms-portal']['jai']['jai_version'].gsub(/\./, "_")
+jai_imageio_version  = node['unredd-nfms-portal']['jai']['jai_imageio_version']
+
+# Note: use of underscores instead of dots is not consistent. there might be problems when version changes
+jai_filename         = "jai-#{jai_version}-lib-linux-#{jai_arch}"
+jai_imageio_filename = "jai_imageio-#{jai_imageio_version.gsub(/\./, "_")}-lib-linux-#{jai_arch}"
+
+java_arch            = node['kernel']['machine'] =~ /x86_64/ ? "amd64" : "i386"
+
+jai_url         = "http://download.java.net/media/jai/builds/release/#{jai_version}/#{jai_filename}.tar.gz"
+jai_imageio_url = "http://download.java.net/media/jai-imageio/builds/release/#{jai_imageio_version}/#{jai_imageio_filename}.tar.gz"
+
+
+ark jai_filename do
+  url    jai_url
   path   "/var/tmp"
   action :put
 end
 
-ark 'jai_imageio' do
-  url    node['unredd-nfms-portal']['jai']['jai_imageio_download_url']
+ark jai_imageio_filename do
+  url    jai_imageio_url
   path   "/var/tmp"
   action :put
 end
@@ -13,9 +27,9 @@ end
 execute "install jai" do
   user "root"
   command <<-EOH
-    cp /var/tmp/jai/lib/*.jar #{node['java']['java_home']}/jre/lib/ext
-    cp /var/tmp/jai/lib/*.so #{node['java']['java_home']}/jre/lib/i386
-    cp /var/tmp/jai_imageio/lib/*.jar #{node['java']['java_home']}/jre/lib/ext
-    cp /var/tmp/jai_imageio/lib/*.so #{node['java']['java_home']}/jre/lib/i386
+    cp /var/tmp/#{jai_filename}/lib/*.jar #{node['java']['java_home']}/jre/lib/ext
+    cp /var/tmp/#{jai_filename}/lib/*.so #{node['java']['java_home']}/jre/lib/#{java_arch}
+    cp /var/tmp/#{jai_imageio_filename}/lib/*.jar #{node['java']['java_home']}/jre/lib/ext
+    cp /var/tmp/#{jai_imageio_filename}/lib/*.so #{node['java']['java_home']}/jre/lib/#{java_arch}
   EOH
 end
