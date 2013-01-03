64f1acf83cfd7dc29a0e057b71a09b2978f1d7f0
Stefano Giaccio <sgiaccio@Stefanos-iMac.local>
2013-01-03 11:51:09 +0100

    ajp proxy ports are now parameters in the template

--- cookbooks/unredd-nfms-portal/recipes/apache2_conf.rb
+++ cookbooks/unredd-nfms-portal/recipes/apache2_conf.rb
@@ -5,13 +5,23 @@ include_recipe "apache2::mod_proxy_ajp"
 # configure mod_proxy
 template "#{node['apache']['dir']}/mods-available/proxy.conf" do
   source "proxy.conf.erb"
-  notifies :restart, "service[apache2]", :delayed
   mode 0644
+
+  notifies :restart, "service[apache2]", :delayed
 end
 
-web_app "tomcat_proxy" do
-  template "unredd_proxy_template.conf.erb"
-  owner "apache"
+template "#{node['apache']['dir']}/sites-enabled/unredd.conf" do
+  source "unredd.conf.erb"
+  variables({
+    :stg_geostore_ajp_porty   => node['unredd-nfms-portal']['stg_geostore']['tomcat_ajp_port'],
+    :stg_geoserver_ajp_porty  => node['unredd-nfms-portal']['stg_geoserver']['tomcat_ajp_port'],
+    :stg_geobatch_ajp_porty   => node['unredd-nfms-portal']['stg_geobatch']['tomcat_ajp_port'],
+    :admin_ajp_porty          => node['unredd-nfms-portal']['stg_geoserver']['tomcat_ajp_port'],
+    :diss_geostore_ajp_porty  => node['unredd-nfms-portal']['diss_geostore']['tomcat_ajp_port'],
+    :diss_geoserver_ajp_porty => node['unredd-nfms-portal']['diss_geoserver']['tomcat_ajp_port'],
+    :portal_ajp_porty         => node['unredd-nfms-portal']['diss_geoserver']['tomcat_ajp_port']
+  })
+  mode 0644
+
   notifies :restart, "service[apache2]", :delayed
-  #mode 0750
 end
