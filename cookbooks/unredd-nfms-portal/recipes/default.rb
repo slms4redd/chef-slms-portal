8d853918a30f17e6b31fbf4e8fb4c7c75726f11f
Stefano Giaccio <sgiaccio@Stefanos-iMac.local>
2013-01-04 14:13:02 +0100

    Deleted duplicate jvm_opts - closes #11

--- cookbooks/unredd-nfms-portal/recipes/default.rb
+++ cookbooks/unredd-nfms-portal/recipes/default.rb
@@ -18,8 +18,8 @@ diss_geoserver_tomcat = node['unredd-nfms-portal']['diss_geoserver']['tomcat_ins
 t = resources(:tomcat => "diss_geoserver")
 t.jvm_opts += [
   "-DPORTAL_CONFIG_DIR=#{node['unredd-nfms-portal']['portal']['config_dir']}",
-  "-DMINIFIED_JS=#{node['unredd-nfms-portal']['portal']['minified_js']}",
-  "-Duser.timezone=GMT"
+  "-DMINIFIED_JS=#{node['unredd-nfms-portal']['portal']['minified_js']}"
+  # "-Duser.timezone=GMT" # this has already been set
 ]
 
 portal_config_dir = node['unredd-nfms-portal']['portal']['config_dir']
@@ -74,7 +74,7 @@ admin_file_name = admin_download_url.split('/')[-1]
 
 # Reopen the stg_geoserver tomcat resource and add admin ui jvm options
 t = resources(:tomcat => "stg_geoserver")
-t.jvm_opts += ["-server -Duser.timezone=GMT"]
+# t.jvm_opts += ["-server -Duser.timezone=GMT"] # already set
 
 # Download the file only if the remote source has changed (uses http_request resource)
 remote_file "/var/tmp/#{admin_file_name}" do
