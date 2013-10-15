2e246ef5ed670e0aaedcefee8fffbbb32826d2f7
Damiano <damianofds@gmail.com>
2013-10-15 18:39:03 +0200

    changed the Readme and add files for run chef with a custom VM

---
+++ customVMfiles/solo.rb
@@ -0,0 +1,15 @@
+file_cache_path "/var/vagrant-cache"
+cookbook_path ["/var/chef-solo/cookbooks"]
+role_path nil
+log_level :info
+
+encrypted_data_bag_secret "/tmp/encrypted_data_bag_secret"
+
+
+http_proxy nil
+http_proxy_user nil
+http_proxy_pass nil
+https_proxy nil
+https_proxy_user nil
+https_proxy_pass nil
+no_proxy nil
